97da7fa1bb88117fbe56bc9565bbef798c07de5d
Azamor <luccas.azamor@gmail.com>
2020-01-15 13:18:43 -0300

    added email password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:     'luccasaugusto918@gmail.com',
-    password:      '',
+    password:      'E9wyH5Rsm7LSFBS',
     domain:        'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
