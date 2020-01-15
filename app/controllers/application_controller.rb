c6070b591cb9f83646be22ddf40b9ffa51d70c38
Azamor <luccas.azamor@gmail.com>
2020-01-15 12:52:27 -0300

    pagina concluida

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+
+  def home
+    unless params.dig(:user).nil?
+      email = "Detalhes do pedido:\n\nEmail: "+params[:user][:address]
+
+      jerry = "\njerry: "+params[:jerry][:price]
+      dave = "\ndave: "+params[:dave][:price]
+      bob = "\nbob: "+params[:bob][:price]
+      kevin = "\nkevin: "+params[:kevin][:price]
+      phil = "\nphil: "+params[:phil][:price]
+
+      email_body = email+jerry+dave+bob+kevin+phil
+
+      ActionMailer::Base.mail(from: "me@example.com", to: "luccas.azamor@gmail.com", subject: "Reserva na Minions Store", body: email_body).deliver
+    end
+
+
+  end
+
+end
